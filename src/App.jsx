24b391d714364a7f0bcfa8c28d7b1a5c5e07d7a8
Jeganalagappan <149089899+Jeganalagappan@users.noreply.github.com>
2024-09-02 16:27:12 +0530

    first commit
    hi

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import TopBar from './components/TopBar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  let [todo,setTodo]=useState([
+    {
+    id:1,
+    title:" Office Task-1",
+    description:"This is the description for my First task",
+    status:true
+
+  },
+  {
+    id:2,
+    title:" Office Task-2",
+    description:"This is the description for my Second task",
+    status:false
+    
+  },
+  {
+    id:3,
+    title:" Office Task-3",
+    description:"This is the description for my Third task",
+    status:true
+    
+  }
+])
+let [completed,setCompleted]=useState("All")
+  return <>
+  <TopBar todo={todo} setTodo={setTodo} completed={completed} setCompleted={setCompleted}/>
+  
+  </>
+}
+
+export default App
